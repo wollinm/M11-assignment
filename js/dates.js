a2700e9beb9ef69034af8fe723a604a3aac9edc9
Magdalena Wollin <mwollino@hotmail.com>
2024-01-10 16:46:05 -0800

    updating from other directory

--- js/dates.js
+++ js/dates.js
@@ -22,7 +22,35 @@ console.log("Month is " + myMonth)
  */
 
 //STEP 3
+/* let date3 = prompt("Enter a date:")
+console.log("Date entered " + date3)
+date3 = new Date(date3 + "Z");
+console.log("Date object " + date3)
+let dow = date3.getDay() + 1
+
+console.log("DOW is " + dow)
+if (dow > 5) { 
+    console.log('This date is a weekend')
+} else {
+    console.log('This date is NOT a weekend')
+} */
 
 //STEP 4
+/* 
+let weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let date4 = prompt("Enter a date:")
+console.log("Date entered " + date4)
+date4 = new Date(date4 + "Z");
+console.log("Date object " + date4)
+let dow = date4.getDay() 
+
+console.log("Prior day is " + weekday[dow])
+ */
 
 //STEP 5
+
+/* let date5 = new Date();
+console.log("Date  " + date5)
+strDate = date5.toString().charAt(0).toUpperCase()
+console.log("First char Date is " + strDate)
+ */
