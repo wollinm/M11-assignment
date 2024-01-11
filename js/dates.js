6e53ad59ae07d2ddd3ef1e04603232068db40801
Magdalena Wollin <mwollino@hotmail.com>
2024-01-10 16:48:34 -0800

    synching again

--- js/dates.js
+++ js/dates.js
@@ -2,7 +2,7 @@
 //Assignment Module 11
 
 //STEP 1
-/* let number1 = prompt("Enter a month:")
+let number1 = prompt("Enter a month:")
 number1 = number1 * 1 + 1
 let dateString = '2024-' + number1 //+ '-01'
 console.log('dateString ' + dateString)
@@ -10,7 +10,7 @@ let date = new Date(dateString + "Z");
 console.log('date ' + date)
 let days = date.getDate()
 console.log("Number of days is " + days)
- */
+
 //STEP 2
 
 /* let date2 = prompt("Enter a date:")
